15d41960ce780ab4aabc9e12e5418490014a2d07
fl4me64 <119824603+fl4me64@users.noreply.github.com>
2023-05-15 23:59:41 -0700

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -1,8 +1,14 @@
 class GameStats():
+	"""Track statistics for the game."""
+	
 	def __init__(self, settings):
+		"""Initialize statistics."""
 		self.settings = settings
-		
+		self.reset_stats()
 		self.game_active = False
 	
 	def reset_stats(self):
-		self.health = 100
+		"""Initialize statistics that can change during the game."""
+		self.rocket_health_remaining = self.settings.rocket_health
+		self.score = 0
+		self.game_over = False
